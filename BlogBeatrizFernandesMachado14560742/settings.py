050ec2ea7d03fd5579faa21ee9f2511036ac05a8
Beatriz Fernandes Machado <beatrizfmachado@usp.br>
2025-11-24 14:00:47 -0300

    Adicionar modificações para implantação no Render

--- BlogBeatrizFernandesMachado14560742/settings.py
+++ BlogBeatrizFernandesMachado14560742/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-kl*yq9up^d=ixmamt!1)7x1b)b51b&r*x1hm&e!*3c!!m-l%8p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com/"]
 
 # Application definition
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
