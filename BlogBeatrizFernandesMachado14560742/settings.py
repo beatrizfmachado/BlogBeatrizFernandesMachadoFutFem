9272bf8a73c2a3392535f858b39052446b52380b
Beatriz Fernandes Machado <beatrizfmachado@usp.br>
2025-12-04 00:42:09 -0300

    Adaptar configurações do banco de dados para implantação no Render

--- BlogBeatrizFernandesMachado14560742/settings.py
+++ BlogBeatrizFernandesMachado14560742/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,16 @@ WSGI_APPLICATION = 'BlogBeatrizFernandesMachado14560742.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600,
+    )
 
 
 # Password validation
